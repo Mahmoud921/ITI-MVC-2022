788c8aeddd22375ca5cfd2e7b342bb1082103b19
Mahmoud Ahmed <hoda1892003@gmail.com>
2025-02-11 00:13:19 +0200

    Model DataAnnotations Validation

--- App/Models/Employee.cs
+++ App/Models/Employee.cs
@@ -1,15 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        [RegularExpression(@"[a-z]+\.(jpg|png)")]
         public string? Image { get; set; }
+
         public string Email { get; set; }
+
+        [RegularExpression(@"[0-9]{4}")]
+        [Range(2000, 7000)]
+        [Required]
         public int Salary { get; set; }
+
+        [RegularExpression("(Fayoum|Cairo|Alex|Sohag)")]
         public string? Address { get; set; }
+
         [ForeignKey("Department")]
         public int DeptId { get; set; }
         // Why use virtual ? when create object, dont create new object , it use old reference
