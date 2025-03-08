9d39091f65152bb5d042b07b511e5d8af5b37b64
Mahmoud Ahmed <hoda1892003@gmail.com>
2025-03-08 23:07:13 +0200

    test routing

---
+++ App/Controllers/RouteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class RouteController : Controller
+    {
+        //Test Routing
+        public IActionResult Index(string name)
+        {
+            return Content(name);
+        }
+    }
+}
